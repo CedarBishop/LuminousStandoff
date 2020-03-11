19452de526c3a5510994b15e8238a6c26bf1393d
Tony <ssj.tony89@gmail.com>
2020-03-11 20:14:09 +1100

    shield effect working

---
+++ Assets/Scripts/Abilities/AbilityInitiate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityInitiate : MonoBehaviour
+{
+    public delegate void ButtonClick();
+    public static ButtonClick OnAbilityClick;
+
+    public void OnButtonClick()
+    {
+        OnAbilityClick();
+    }
+}
